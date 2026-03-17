16a1d2b4122f873845dbfa2b15de14b8e5ad57ba
NahuelLuna <nahuel.lunacab@gmail.com>
2026-03-17 17:32:19 -0300

    feat: expone carpeta results como archivos estáticos

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+
 
 app = FastAPI(title="Simulación de Variables Aleatorias")
 
@@ -11,5 +14,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/results", StaticFiles(directory="results"), name="results")
 
 app.include_router(router)
